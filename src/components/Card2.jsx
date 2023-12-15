0bc472d2e5456ae569558dbf04c71387861db3bc
Muhammadullo <mnastulloev@gmail.com>
2023-12-15 10:36:49 +0500

    fix

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Card2 = ({ }) => {
     return (
-        <div className='w-[33%] h-[75vh] border-red-500 border-solid border-[2px]'>
-            <div className='main w-[100%] h-[35vh] flex flex-col  justify-between'>
-                <p className='w-[15%] h-[4vh] bg-[red] rounded-[6px] ml-[15px] text-[white] text-center'>Sale</p>
+        <div className='w-[33%] h-[80vh] border-red-500 border-solid border-[2px]'>
+            <div className='main w-[100%] h-[35vh] flex flex-col  justify-between p-[10px]'>
+                <p className='w-[15%] h-[4vh] bg-[red] rounded-[6px] ml-[15px] font-medium   text-[white] text-center'>Sale</p>
                 <div className='flex justify-center'>
                     <img src='src/assets/l.png'       className='m-[10px]'/>
                     <img src='src/assets/like.png'    className='m-[10px]'/>
