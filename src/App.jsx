cbad58eb70f4b29a97b2c30a50fcc29e91ef476e
Muhammadullo <mnastulloev@gmail.com>
2023-12-15 08:04:18 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
+import CardOne from './components/Card/CardOne';
+
+let data = [{ "name": 1, "title": "Hello, World!", "id": 1 }, { "name": 1, "title": "Hello, World!", "id": 2 } ]
+
+
+function del(id){
+  console.log(id);
+  data = data.filter((e) => {
+    return e.id != id
+  })
+}
 
 function App() {
-
-
   return (
     <>
 
       {/*#header*/}
-      <header className='w-[90%] h-[10vh] border-solid border-red-500 border-[3px] m-auto flex justify-between items-center'>
+      <header className='w-[90%] h-[10vh] border-solid border-red-500 border-[3px] m-auto flex justify-between items-center sm:flex-col
+      lg:flex-row'>
         <h1 className='font-bold text-[25px]'>Your School</h1>
 
-        <ul className='flex'>
+        <ul className='flex md:text-[14px]'>
           <li className='text-[#737373] text-[18px] m-[8px] font-semibold'>Home</li>
           <li className='text-[#737373] text-[18px] m-[8px] font-semibold'>Product</li>
           <li className='text-[#737373] text-[18px] m-[8px] font-semibold'>Pricing</li>
@@ -21,10 +31,8 @@ function App() {
           <button className='text-[#96BB7C]'>Login</button>
           <button className='w-[140px] h-[45px]  rounded-[12px] m-[10px] font-bold bg-[#96BB7C] text-[white]'>JOIN US</button>
         </div>
-
       </header>
-
-
+      
     </>
   )
 }
